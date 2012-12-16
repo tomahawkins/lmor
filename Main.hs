6d1662a2deb927f30e6d99aeae4bbc3e65c02a40
Tom Hawkins <tomahawkins@gmail.com>
2012-12-16 13:15:57 -0600

    Branch targets for call instructions.

--- Main.hs
+++ Main.hs
@@ -1,11 +1,14 @@
 module Main (main) where
 
-import Data.Elf
+import Data.Bits
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
+import Data.Elf
+import Data.Int
+import Data.Maybe
 import Data.Word
-import Harpy.X86Disassembler
 import System.Environment
+import Text.Disassembler.X86Disassembler
 import Text.Printf
 
 main :: IO ()
@@ -20,10 +23,13 @@ analyze file = do
   f <- B.readFile file
   let (text, addr, reconstruct) = extractText f
   printf "%08x\n" addr
+  print $ length $ branchTargets text
+  {-
   r <- disassembleList (B.unpack $ B.take 100 text)
   case r of
     Left e -> print e
     Right i -> mapM_ print i
+    -}
 
 -- | Return the .text section, the address of the section, and a function to reconstruct the elf file with a modified .text section.
 extractText :: ByteString -> (ByteString, Word64, ByteString -> ByteString)
@@ -37,4 +43,17 @@ extractText file = (textData, elfSectionAddr text, reconstruct)
   (before, rest) = B.breakSubstring textData file
   reconstruct text = B.concat [before, text, B.drop (B.length textData) rest]
 
-
+-- | Possible branch targets.
+branchTargets :: ByteString -> [Int]
+branchTargets program = mapMaybe f [0 .. B.length program - 1]
+  where
+  f :: Int -> Maybe Int
+  f i
+    | B.index program i == 0xe8 && i < B.length program - 4 && address >= 0 && address < B.length program = Just address
+    | otherwise = Nothing
+    where
+    bytes = [ B.index program $ i + j | j <- [4, 3 .. 1] ]
+    word :: Word32
+    word = foldl1 (.|.) [ shiftL (fromIntegral b) s | (b, s) <- zip bytes [24, 16 .. 0] ]
+    address = (fromIntegral (fromIntegral word :: Int32) :: Int) + i + 1
+    
